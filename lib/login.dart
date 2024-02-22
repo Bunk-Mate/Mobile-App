e82301b7e1c58f152bd9dd28dd2576f7415aa11c
hridesh <hridesh699@gmail.com>
2024-02-22 18:00:30 +0530

    fix: improved sidebar UI

--- lib/login.dart
+++ lib/login.dart
@@ -63,164 +63,163 @@ class _LoginPageState extends State<LoginPage> {
             Color.fromARGB(255, 7, 9, 15)
           ])),
       child: Scaffold(
-          backgroundColor: Color.fromARGB(255, 7, 9, 15),
-          body: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: [
-                  Container(
-                      decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                            Color.fromARGB(255, 13, 15, 21),
-                            Color.fromARGB(255, 7, 9, 15)
-                          ])),
-                      // color: Color.fromARGB(255, 13, 15, 21),
+        backgroundColor: Color.fromARGB(255, 7, 9, 15),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Color.fromARGB(255, 13, 15, 21),
+                          Color.fromARGB(255, 7, 9, 15)
+                        ])),
+                    // color: Color.fromARGB(255, 13, 15, 21),
 
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SafeArea(
-                                  child: Text("Sign in to your account",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 40,
-                                          fontFamily: 'alpha'))),
-                            ),
-                          ],
-                        ),
-                      )),
-                  Padding(
-                      padding: EdgeInsets.all(12),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
                       child: Column(
                         children: [
-                          SizedBox(
-                            width: 55,
-                            height: 55,
-                          ),
-                          TextField(
-                            controller: UserNameController,
-                            style: TextStyle(color: Colors.white),
-                            decoration: const InputDecoration(
-                                hintText: 'Username',
-                                border: OutlineInputBorder(),
-                                hintStyle: TextStyle(color: Colors.white),
-                                filled: true,
-                                fillColor: Color.fromARGB(255, 17, 20, 27)),
-                          ),
-                          SizedBox(
-                            width: 25,
-                            height: 25,
-                          ),
-                          TextField(
-                            controller: PasswordController,
-                            decoration: const InputDecoration(
-                                hintText: 'Password',
-                                border: OutlineInputBorder(),
-                                hintStyle: TextStyle(color: Colors.white),
-                                filled: true,
-                                fillColor: Color.fromARGB(255, 17, 20, 27)),
-                            obscureText: true,
-                            style: TextStyle(
-                              color: Colors
-                                  .white, // This sets the color of the text
-                            ),
-                          ),
-                          SizedBox(
-                            width: 50,
-                            height: 10,
-                          ),
-                          GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => Navigation()));
-                              },
-                              child: Align(
-                                  alignment: Alignment.topRight,
-                                  child: Text(
-                                    "Forgot Password ?",
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SafeArea(
+                                child: Text("Sign in to your account",
                                     style: TextStyle(
                                         color: Colors.white,
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 15),
-                                  ))),
-                          SizedBox(
-                            width: 50,
-                            height: 20,
+                                        fontSize: 40,
+                                        fontFamily: 'alpha'))),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                                onTap: () {
-                                  sendPostRequest().then(
-                                    (response) => {
-                                      if (response.statusCode == 202)
-                                        {
-                                          Navigator.pushNamed(
-                                              context, '/mainPage')
-                                        }
-                                    },
-                                  );
-                                },
-                                child: Container(
-                                  child: Center(
-                                      child: Text(
-                                    "Login",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'alpha'),
-                                  )),
-                                  width: 405,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.white,
-                                  ),
-                                )),
+                        ],
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: 55,
+                          height: 55,
+                        ),
+                        TextField(
+                          controller: UserNameController,
+                          style: TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                              hintText: 'Username',
+                              border: OutlineInputBorder(),
+                              hintStyle: TextStyle(color: Colors.white),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 17, 20, 27)),
+                        ),
+                        SizedBox(
+                          width: 25,
+                          height: 25,
+                        ),
+                        TextField(
+                          controller: PasswordController,
+                          decoration: const InputDecoration(
+                              hintText: 'Password',
+                              border: OutlineInputBorder(),
+                              hintStyle: TextStyle(color: Colors.white),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 17, 20, 27)),
+                          obscureText: true,
+                          style: TextStyle(
+                            color:
+                                Colors.white, // This sets the color of the text
                           ),
-                          Column(
-                            children: [
-                              Container(
+                        ),
+                        SizedBox(
+                          width: 50,
+                          height: 10,
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => Navigation()));
+                            },
+                            child: Align(
+                                alignment: Alignment.topRight,
                                 child: Text(
-                                  "Don't have an account",
+                                  "Forgot Password ?",
                                   style: TextStyle(
                                       color: Colors.white,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 25,
-                                height: 2,
-                              ),
-                              GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                SignupPage()));
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15),
+                                ))),
+                        SizedBox(
+                          width: 50,
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                              onTap: () {
+                                sendPostRequest().then(
+                                  (response) => {
+                                    if (response.statusCode == 202)
+                                      {
+                                        Navigator.pushNamed(
+                                            context, '/mainPage')
+                                      }
                                   },
-                                  child: Container(
-                                    child: Text("Signup ? ",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold)),
-                                  )),
-                            ],
-                          )
-                        ],
-                      ))
-                ],
-              ),
+                                );
+                              },
+                              child: Container(
+                                child: Center(
+                                    child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'alpha'),
+                                )),
+                                width: 405,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white,
+                                ),
+                              )),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              child: Text(
+                                "Don't have an account",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 25,
+                              height: 2,
+                            ),
+                            GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => SignupPage()));
+                                },
+                                child: Container(
+                                  child: Text("Signup ? ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold)),
+                                )),
+                          ],
+                        )
+                      ],
+                    ))
+              ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
