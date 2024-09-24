e8da4d63882b05c62e4884408dee2dd307ad212a
angrezichatterbox <gouthammohanraj@gmail.com>
2024-09-24 21:33:03 +0530

    fix:Fixed critical bugs

--- lib/controllers/homepage/course_summary_controller.dart
+++ lib/controllers/homepage/course_summary_controller.dart
@@ -1,21 +1,22 @@
 import 'dart:convert';
 import 'package:bunk_mate/models/course_summary_model.dart';
-import 'package:bunk_mate/screens/OnBoardView.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:bunk_mate/utils/api_endpoints.dart';
 import 'package:http/http.dart' as http;
 import 'dart:core';
 import 'dart:io';
+import 'package:bunk_mate/screens/OnBoardView.dart';
 
 class CourseSummaryController extends GetxController {
   var courseSummary = <CourseSummary>[].obs;
   final storage = const FlutterSecureStorage();
 
+  var checkIn = false.obs;
   Future<String?> getToken() async {
     return await storage.read(key: 'token');
   }
-var checkIn = false.obs;
+
   Future<void> fetchCourseSummary() async {
     final token = await getToken();
     if (token == null) {
@@ -30,34 +31,22 @@ var checkIn = false.obs;
       var url = Uri.parse(
           ApiEndPoints.baseUrl + ApiEndPoints.homeEndPoints.courseSummary);
       http.Response response = await http.get(url, headers: headers);
-      response = http.Response(response.body, 200);
+      print(response.statusCode);
       if (response.statusCode == 200) {
-        
-        List<dynamic> jsonData = json.decode(response.body);
-        var fetchedSummary =
-            jsonData.map((data) => CourseSummary.fromJson(data)).toList();
+        final Map<String, String> responseMap = json.decode(response.body);
+        print(responseMap);
+          checkIn.value = true;
+          List<dynamic> jsonData = json.decode(response.body);
+          var fetchedSummary =
+              jsonData.map((data) => CourseSummary.fromJson(data)).toList();
+          print(response.body);
+          print(jsonData[0]);
 
-        courseSummary.assignAll(fetchedSummary);
-        final Map<String, dynamic> responseMap = json.decode(response.body);
-         if (responseMap['detail'] == "Not found.") {
-        checkIn.value = false ; 
-    }
-    else {
-      checkIn.value = true;
-      
-    }
-if (checkIn.value == false) {
-Future.delayed(const Duration(seconds: 3), () {
-      Get.offAll(TimetableView());
-      checkIn.value == true ;
-    });
-    }      
-       print(response.body);
-        
-        print("Task Done");  
-      } else {
-        var errorResponse = jsonDecode(response.body);
-        print(response.statusCode);
+          courseSummary.assignAll(fetchedSummary);
+          print("Task Done");
+        }
+      else {
+        Get.off(TimetableView());
       }
     } catch (error) {
       print(error);
